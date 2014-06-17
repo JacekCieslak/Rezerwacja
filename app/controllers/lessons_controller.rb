2fe405362b7758358c720791e8e5822b57a95dbd
Szymon Starzak <szymonstarzak@gmail.com>
2014-06-17 14:29:53 +0200

    Dodawanie nowych zajec (controller + model + view)

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,2 +1,15 @@
 class LessonsController < ApplicationController
+	def new
+		@lesson = Lesson.new
+	end
+	def create
+		@lesson = Lesson.new(lesson_params)
+		@lesson.employee_id = current_user.profile.id
+		@lesson.save
+	end
+
+	def lesson_params
+		params.require(:lesson).permit(:name, :hour,:week_type,:day, :room_id )
+	end
 end
+
