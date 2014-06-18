efd7789b82306296554506bb40a91358e218976f
Jacek Cielak <jacekcieslakpl@gmail.com>
2014-06-18 12:16:38 +0200

    Uzupełnie menu

--- db/migrate/20140618075640_table_changes.rb
+++ db/migrate/20140618075640_table_changes.rb
@@ -1,6 +1,5 @@
 class TableChanges < ActiveRecord::Migration
   def change
-   drop_table :less_studs
    remove_column :lessons, :student_id
    create_table :lessons_students, id: false do |t|
       t.belongs_to :student
