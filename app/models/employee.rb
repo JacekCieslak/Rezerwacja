58122a16a5fcaad1eae2bdb3a7ddd536d8c793c2
Szymon Starzak <szymonstarzak@gmail.com>
2014-06-23 23:48:16 +0200

    Poprawki w widoku zajec i zakonczenie sprawozdania

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,7 +1,9 @@
 class Employee < ActiveRecord::Base
 	attr_accessor :email,:password
+
 	has_one :user, :as => :profile
 	has_many :lessons
+	
 	validates :name, :surname, :degree, :email, :password, presence: true
-	 validates :name, :surname, length: { minimum: 3 }, :presence => { :message => " cannot be blank" }
+	validates :name, :surname, length: { minimum: 3 }, :presence => { :message => " cannot be blank" }
 end
